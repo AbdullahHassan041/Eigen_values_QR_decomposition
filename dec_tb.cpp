af30e82b9391a17ca18a9ab02b29b609575ef356
Abdullah hassan <95423470+AbdullahHassan041@users.noreply.github.com>
2022-06-01 10:41:15 +0500

    Update dec_tb.cpp

--- dec_tb.cpp
+++ dec_tb.cpp
@@ -4,22 +4,18 @@ using namespace hls;
 int main ()
 {
 
-	matrix_t array[Inp_matrix] = { 3,2,1,4};
-	matrix_t tem_input_2d_array[matrix_size][matrix_size] = {0};
-	tem_input_2d_array[0][0]=array[0];
-	tem_input_2d_array[0][1]=array[1];
-	tem_input_2d_array[1][0]=array[2];
-	tem_input_2d_array[1][1]=array[3];
+	matrix_t array[row][column] = {{1,1,1,1,1,1,1,1,1,0},{1,1,1,1,1,1,1,1,0,1},{1,1,1,1,1,1,1,0,1,1},{1,1,1,1,1,1,0,1,1,1},{1,1,1,1,1,0,1,1,1,1},
+									{1,1,1,1,1,0,1,1,1,1},{1,1,1,0,1,1,1,1,1,1},{1,1,0,1,1,1,1,1,1,1},{1,0,1,1,1,1,1,1,1,1},{0,1,1,1,1,1,1,1,1,1}};
 
 	printf("                                           OUR INPUT MATRIX\n");
-	for(int i=0;i<matrix_size;i++)
+	for(int i=0;i<row;i++)
 	{
-		for(int j=0;j<matrix_size;j++)
+		for(int j=0;j<column;j++)
 		{
-			printf("%1.4f ",tem_input_2d_array[i][j]);
+			printf("%1.4f ",array[i][j]);
 		}
 		printf("\n");
 	}
-	decomposition(array);
+	decompose(array);
 	return 0;
 }
