d25c5b0558e6538cd6dab405a9140ec63c054f54
Abdullah hassan <95423470+AbdullahHassan041@users.noreply.github.com>
2022-06-01 10:40:41 +0500

    Update dec.h

--- dec.h
+++ dec.h
@@ -1,15 +1,17 @@
-
 #include "hls_stream.h"
 #include "hls_linear_algebra.h"
-//#include "hls_math.h"
+#include "hls_math.h"
 //#include "hls_matrix_multiply.h"
 #include "math.h"
 #include "stdio.h"
 #include "stdint.h"
-#define matrix_size 2
-#define Inp_matrix 4
+#define matrix_size 10
+#define row 10
+#define column 10
+#define Inp_matrix 9
 using namespace hls;
 
 typedef float matrix_t;
-void decomposition(matrix_t *A);
+void decompose(matrix_t input[row][column]);
+
 
